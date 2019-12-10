44c9dbad3ad8f31b6c93348daa368a1642162979
Jacob Faulk <jacob5567@gmail.com>
2019-12-09 16:50:22 -0800

    adding standard assets and simple boid movement

--- Assets/Scripts/BoidScript.cs
+++ Assets/Scripts/BoidScript.cs
@@ -9,12 +9,37 @@ public class BoidScript : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.velocity = Vector3.forward * 5f;
+        rb.velocity = transform.forward * 15f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        LoopAroundArea();
+    }
 
+    private void LoopAroundArea()
+    {
+        float x, y, z;
+        if (transform.position.x > 100)
+            x = 0;
+        else if (transform.position.x < 0)
+            x = 100;
+        else
+            x = transform.position.x;
+        if (transform.position.y > 100)
+            y = 0;
+        else if (transform.position.y < 0)
+            y = 100;
+        else
+            y = transform.position.y;
+        if (transform.position.z > 100)
+            z = 0;
+        else if (transform.position.z < 0)
+            z = 100;
+        else
+            z = transform.position.z;
+
+        transform.position = new Vector3(x, y, z);
     }
 }
---
+++ Assets/Scripts/FlockGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockGenerator : MonoBehaviour
+{
+    public GameObject BoidPrefab;
+    public int numBoids;
+    private List<GameObject> boidsList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numBoids; i++)
+        {
+            boidsList.Add(Instantiate(BoidPrefab, new Vector3(Random.Range(0, 100),
+                Random.Range(0, 100), Random.Range(0, 100)), Random.rotation));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
