0e8dc0f9d1e3101780dc49523078a7177c38ea82
Jacob Faulk <jacob5567@gmail.com>
2019-12-09 16:09:01 -0800

    added standard assets and simple boid movement

---
+++ Assets/Scripts/BoidScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = Vector3.forward * 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
